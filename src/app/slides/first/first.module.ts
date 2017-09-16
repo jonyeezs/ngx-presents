851141417ac0f1e726eb4c9c3bd92b96d1ab54fe
jonyeezs <jonyeezs@gmail.com>
2017-09-17 00:35:37 +1000

    add transition to first slide as demo

--- src/app/slides/first/first.module.ts
+++ src/app/slides/first/first.module.ts
@@ -2,11 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FirstComponent } from './first.component';
 
+import { TransitionDirective } from '../../plugins/transition/transition.directive';
+
+
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [FirstComponent],
+  declarations: [FirstComponent, TransitionDirective],
   exports: [FirstComponent]
 })
 export class FirstModule { }
